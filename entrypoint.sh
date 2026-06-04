bf6e6dbcea3b966e4f52382f10ae5cb77d7b5415
max <maxandreev27@gmail.com>
2026-06-04 15:03:17 +0300

    Fixtures to production

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,8 @@ set -e
 echo "==> Running database migrations on persistent volume..."
 python manage.py migrate --noinput
 
+echo "==> Loading initial subjects fixtures..."
+python manage.py loaddata subjects.json
+
 echo "==> Starting Gunicorn server..."
 exec gunicorn --bind 0.0.0.0:8000 --workers 2 config.wsgi
