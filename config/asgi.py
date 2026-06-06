3715cee1c08f2ba7d045f46c53be9e0875dde85a
max <maxandreev27@gmail.com>
2026-06-06 19:07:47 +0300

    Deploy fix

--- config/asgi.py
+++ config/asgi.py
@@ -16,7 +16,7 @@ from django.core.asgi import get_asgi_application
 
 import chat.routing
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "educa.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
 django_asgi_app = get_asgi_application()
 
